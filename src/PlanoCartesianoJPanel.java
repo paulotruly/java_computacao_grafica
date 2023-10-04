74b8e0cfc7fafe013a200f73bc896d0d2f171734
Paulo <paulomsmsm@gmail.com>
2023-10-03 21:10:02 -0300

    update031023

--- src/PlanoCartesianoJPanel.java
+++ src/PlanoCartesianoJPanel.java
@@ -1,27 +1,20 @@
 // BASE: Fig. 12.18: Bandeira
 import java.awt.Color;
 import java.awt.Graphics;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class PlanoCartesianoJPanel extends JPanel 
-{
-   // Emoji
-   public void paintComponent( Graphics g )
-   {
-      super.paintComponent( g ); // chama o método paint da superclasse
+public class PlanoCartesianoJPanel extends JPanel {
 
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g); // chama o método paint da superclasse
       this.setBackground( Color.WHITE );
-    
-    g.setColor(new Color(0,0,0));
     
     // tracinhos x pro y
     g.drawLine(299, 35, 323, 35);
     g.drawLine(299, 135, 323, 135);
-    
     g.drawLine(299, 405, 323, 405);
-    g.drawLine(299, 305, 323, 305);
-   
-
+    g.drawLine(299, 305, 323, 305);  
     // linha y
      g.drawLine(312, 0, 312, 440);
     //tracinhos y pro x
@@ -34,14 +27,25 @@ public class PlanoCartesianoJPanel extends JPanel
      g.drawLine(403, 210, 403, 230);   
     // linha x
      g.drawLine(0, 220, 623, 220);
-   
-// 624x440
-// 312x220
-
-    //  320     0    320   480
-     // int x int y int2 x int2 y
-    //  0      320   480   320
-   
+    // tracinho vermelho
+     g.setColor(Color.RED);
+     g.drawLine(505, 30, 400, 140);
+   // i x, i y - f x, f y
       
+     g.setColor(Color.BLUE);
+     g.drawString("2", 280, 40);
+     g.drawString("1", 280, 140);
+     g.drawString("-1", 280, 310);
+     g.drawString("-2", 280, 410);
+   
+     g.drawString("-3", 12, 250);
+     g.drawString("-2", 112, 250);
+     g.drawString("-1", 212, 250);
+     g.drawString("0", 282, 250);
+     g.drawString("1", 400, 250);
+     g.drawString("2", 500, 250);
+     g.drawString("3", 600, 250);
+
+
    } // fim do m�todo paintComponent
 } // fim da classe BandeiraJPanel
